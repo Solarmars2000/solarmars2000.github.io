870cab5398cb5a3e66be7c1e49ef7e02e39539ae
Prasad <prasadpeebles@gmail.com>
2018-02-19 01:40:53 +0000

    Add gemetric_pattern  project

--- that_was_easy/main.js
+++ that_was_easy/main.js
@@ -8,7 +8,8 @@ $("#easy").on("click", sayThatWasEasy);
 $(document).keypress(delegateKeypress);
 
 function delegateKeypress(event) { 
-    if (event.charCode == 32) {
+    if (event.char<textarea rows="4" cols="50">
+
         $("#easy").trigger("click");
     }
 }
